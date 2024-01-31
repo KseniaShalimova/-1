9a2d9cc444f458b4d4a19c5d1e58db3bdf8a8a17
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-31 20:53:01 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿string str = "qqwewqq";
+string answer = "Yes";
+for (int i = 0; i < str.Length / 2; i++)
+{
+    if (str[i] != str[str.Length - i - 1])
+    {
+        answer = "No";
+    }
+}
+Console.WriteLine(answer);
