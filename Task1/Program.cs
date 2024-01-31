2fcff10fb22a69b0e1a2f70bf60cb0a9a765df7c
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-01-31 19:37:24 +0500

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿string GetStringFromCharMatrix(char[,] matrix)
+{
+    string str = "";
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            str = str + matrix[i, j];
+        }
+    }
+    return str;
+}
+
+
+
+char[,] matrix = {
+    {'q','w','e'},
+    {'r','t','y'},
+    {'a','s','d'}
+};
+string answer = GetStringFromCharMatrix(matrix);
+Console.Write(answer);
